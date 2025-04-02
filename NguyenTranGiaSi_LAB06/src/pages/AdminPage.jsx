8609017e2afbb82ea7a0f7f77616132888c0124a
H82M19\Student <Student>
2025-04-02 17:33:52 +0700

    Create layout for admin page

---
+++ NguyenTranGiaSi_LAB06/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashBoard from '../components/MainLayout';
+
+function AdminPage() {
+  return (
+    <div>
+        <DashBoard />
+    </div>      
+  );
+}
+
+export default AdminPage;
