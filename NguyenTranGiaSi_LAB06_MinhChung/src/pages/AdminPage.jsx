10a4abbad170b8ada94ad3ed9b6f36596aefa5ce
H82M19\Student <Student>
2025-04-09 16:10:31 +0700

    ADD: 1. Create Layout

---
+++ NguyenTranGiaSi_LAB06_MinhChung/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/mainLayout';
+
+function AdminPage() {
+  return (
+    <div>
+        <Dashboard />
+    </div>      
+  );
+}
+
+export default AdminPage;
